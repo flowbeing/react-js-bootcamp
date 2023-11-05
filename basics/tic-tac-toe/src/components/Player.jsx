0b48c87c9e85402f4da42afa4ab582b119f80400
admin <flowbeing@proton.me>
2023-11-05 20:36:09 +0000

    nodejs bootcamp: added player name saving and editing functionality to tic-tac-toe web app

--- basics/tic-tac-toe/src/components/Player.jsx
+++ basics/tic-tac-toe/src/components/Player.jsx
@@ -1,13 +1,42 @@
+import {useState} from 'react';
+
 export default function Player({playerName, symbol}){
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [inputContent, updateInputContent] = useState(playerName);
+
+    let buttonContent = 'Edit';
+    let playerNameElement = <span className="player-name">{inputContent}</span>;
+
+    function handleButtonClick(){
+        setIsEditing(editing => !editing);
+    }
+
+    function handlePlayerNameChange(event){
+
+        // current value of playerName per user input
+        let currentPlayerName = event.target.value;
+        updateInputContent(previousInputContent => currentPlayerName);
+    }
+
+    if (isEditing){
+        buttonContent = 'Save';
+        playerNameElement = <input className='player input' type="text" defaultValue={playerName} value={inputContent} onChange={handlePlayerNameChange}></input>;
+    }
+    else if (!isEditing){
+        buttonContent = 'Edit';
+    }
+
+
     return (
         <li>
             <span className="player">
 
-                <span className="player-name">{playerName}</span>
+                {playerNameElement}
                 <span className="player-symbol">{symbol}</span>
 
             </span>
-            <button>Edit</button>
+            <button onClick={handleButtonClick}>{buttonContent}</button>
             
         </li>
     );
