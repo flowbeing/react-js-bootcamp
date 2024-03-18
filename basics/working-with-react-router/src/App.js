a0f874c150081ac14016ce856c44b2bb906bfb26
admin <flowbeing@proton.me>
2024-03-18 23:13:56 +0000

    worked with react's 'lazy' hook

--- basics/working-with-react-router/src/App.js
+++ basics/working-with-react-router/src/App.js
@@ -1,17 +1,36 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { lazy } from "react";
 
 import RootLayout from "./pages/RootLayout";
 import ErrorPage from "./pages/ErrorPage";
 import Homepage from "./pages/Homepage";
 import EventsPage, { eventsPageAction } from "./pages/EventsPage";
+
 import RegistrationPage, {
   registrationPageLoaderFunction,
   registrationPageActionFunction,
 } from "./pages/RegistrationPage";
-import MyDetailsPage, {
-  myDetailsActionFunction,
-  myDetailsLoaderFunction,
-} from "./pages/MyDetailsPage";
+
+// import MyDetailsPage, {
+//   myDetailsActionFunction,
+//   myDetailsLoaderFunction,
+// } from "./pages/MyDetailsPage";
+
+import LoginPage, {
+  loginPageLoaderFunction,
+  loginPageActionFunction,
+} from "./pages/LoginPage";
+
+// import LoginOrSignupPage, {
+//   getLoginOrRegistrationLoaderFunction,
+//   getLoginOrRegistrationActionFunction,
+// } from "./pages/LoginOrSignupPage";
+
+const LoginOrSignupPage = lazy(() => import("./pages/LoginOrSignupPage"));
+console.log(`LoginOrSignupPage: ${LoginOrSignupPage}`);
+
+const MyDetailsPage = lazy(() => import("./pages/MyDetailsPage"));
+console.log(`MyDetailsPage: ${MyDetailsPage}`);
 
 const router = createBrowserRouter([
   {
@@ -26,17 +45,37 @@ const router = createBrowserRouter([
       },
       { path: "events", element: <EventsPage />, action: eventsPageAction },
       {
-        path: "register",
-        element: <RegistrationPage />,
-        // loader: registrationPageLoaderFunction
-        action: registrationPageActionFunction,
+        path: "/:authMode",
+        element: <LoginOrSignupPage />,
+        // LoginOrSignupPage === "login" ? <LoginPage /> : <RegistrationPage />,
+        // STOPPED HERE -> Change
+        loader: ({ request, params }) =>
+          import("./pages/LoginOrSignupPage").then((module) =>
+            module.getLoginOrRegistrationLoaderFunction({ request, params })
+          ), // getLoginOrRegistrationLoaderFunction,
+        action: ({ request, params }) =>
+          import("./pages/LoginOrSignupPage").then((module) =>
+            module.getLoginOrRegistrationActionFunction({ request, params })
+          ), // getLoginOrRegistrationActionFunction,
       },
       {
         path: "my-details",
         element: <MyDetailsPage />,
-        loader: myDetailsLoaderFunction,
-        action: myDetailsActionFunction,
+        loader: ({ request, params }) =>
+          import("./pages/MyDetailsPage").then((module) =>
+            module.myDetailsLoaderFunction({ request, params })
+          ), // myDetailsLoaderFunction
+        action: ({ request, params }) =>
+          import("./pages/MyDetailsPage").then((module) =>
+            module.myDetailsActionFunction({ request, params })
+          ), // myDetailsActionFunction,
       },
+      // {
+      //   path: "my-details/:id",
+      //   element: <MyDetailsPage />,
+      //   loader: myDetailsLoaderFunction,
+      //   action: myDetailsActionFunction,
+      // },
     ],
   },
 ]);
