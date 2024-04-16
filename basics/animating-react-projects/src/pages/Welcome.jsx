979326fbd1410cd24fff6f85246847b9614a40c2
admin <flowbeing@proton.me>
2024-04-16 23:21:39 +0100

    externalized the framer-motion's component transition property

--- basics/animating-react-projects/src/pages/Welcome.jsx
+++ basics/animating-react-projects/src/pages/Welcome.jsx
@@ -101,9 +101,10 @@ export default function WelcomePage() {
 
       <motion.div 
         style={{ width: "200px", height: "200px", backgroundColor: "white", margin: "600px auto 600px" }}
-        variants={{ animate: { x: 500, y: [-250, 250, -250, 250, -250, 250, -250, 250, -250, 250, -250, 250], rotate: 360, transition: { duration: 6  } } }}
+        variants={{ animate: { x: 500, y: [-250, 250, -250, 250, -250, 250, -250, 250, -250, 250, -250, 250], rotate: 360 } }}
         initial={{ x: -500, y: -250, rotate: 0 }}
         animate="animate"
+        transition= {{ duration: 6  }}
       />
       
       <main id="welcome-content">
