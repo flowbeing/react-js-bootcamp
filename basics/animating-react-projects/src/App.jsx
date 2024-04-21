b857ee555e25b3ae74a344d5cf003b4d93b22c77
admin <flowbeing@proton.me>
2024-04-21 23:30:22 +0100

    included an error page for react router's index page

--- basics/animating-react-projects/src/App.jsx
+++ basics/animating-react-projects/src/App.jsx
@@ -2,10 +2,17 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import WelcomePage from './pages/Welcome.jsx';
 import ChallengesPage from './pages/Challenges.jsx';
+import ErrorPage from "./pages/ErrorPage.jsx";
 // import TabsPage from "./pages/TabsPage.jsx";
 
 const router = createBrowserRouter([
-  { path: '/', element: <WelcomePage /> },
+  { path: '/', 
+    element: <WelcomePage />,
+    id: "rootId",
+    errorElement: <ErrorPage/>
+    // loader: noLoaderFunction
+    // action: noActionFunction
+  },
   { path: '/challenges', element: <ChallengesPage /> },
 ]);
 
